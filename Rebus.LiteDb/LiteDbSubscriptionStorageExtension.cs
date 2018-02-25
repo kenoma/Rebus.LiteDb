171c22f4fd9a8f1a55662038251511b25b2aac4e
bghati@yandex.ru <bghati@yandex.ru>
2018-02-25 15:48:49 +0300

    Add LiteDbSubscriptionStorageExtension

---
+++ Rebus.LiteDb/LiteDbSubscriptionStorageExtension.cs
@@ -0,0 +1,26 @@
+﻿using Rebus.Config;
+using Rebus.Subscriptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rebus.LiteDb
+{
+    public static class LiteDbSubscriptionStorageExtension
+    {
+        /// <summary>
+        ///  Configures Rebus to store subscriptions in LiteDb databse. Database file should be shard between all participants.
+        /// </summary>
+        public static void StoreInLiteDb(this StandardConfigurer<ISubscriptionStorage> configurer, string pathToDatabase)
+        {
+            if (configurer == null)
+                throw new ArgumentNullException(nameof(configurer));
+            if (string.IsNullOrEmpty(pathToDatabase))
+                throw new ArgumentNullException(nameof(pathToDatabase));
+
+            configurer.Register(c => new LiteDbSubscriptionStorage(pathToDatabase, new CollectionNameCorrector()));
+        }
+    }
+}
